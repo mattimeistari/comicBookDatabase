c9372c3cf10807c7ca3d944f646926bdd31543ff
Matthías Madsen Hauksson - FB <FB687789@fb.is>
2023-09-11 12:49:58 +0000

    First commit

---
+++ python/main.py
@@ -0,0 +1,49 @@
+import sqlite3
+
+
+# Create placeholder
+def placeholderText():
+    print("WTF!")
+    return
+
+
+# Connect comicBook.db
+conn = sqlite3.connect("./db/comicBook.db")
+
+# Create a cursor object
+cursor = conn.cursor()
+
+# Prompt the user to print out the comic books
+print("Do you wanna print all the peopls? (Y/N)")
+choice = input("\nAnswer here:")
+
+# If the user chooses to print the comic books
+if choice.lower() == "y":
+    # Execute SELECT statement
+    cursor.execute(
+        "SELECT userName, firstName, lastName, email, flags "
+        "FROM comics;"
+    )
+
+    # Fetch all the rows and display the names
+    rows = cursor.fetchall()
+    for row in rows:
+        print(
+            f"Name: {row[0]}, Full name: {row[1]} {row[2]}, "
+            f"Email: {row[3]} Flag value: {row[4]}\n"
+        )
+
+# If he chooses not to, run option 2, which is placeholder
+elif choice.lower() == "n":
+    placeholderText()
+
+# If user fails to inster y/n
+else:
+    print("User Failed to follow instructions")
+
+# End the forrit
+cursor.close()
+conn.close()
+
+# Closing message
+print("######## FORRIT END ########")
